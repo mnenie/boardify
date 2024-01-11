ddb0c01318ad734ad618312accbf9ec5c864f3ca
mnenie <121057011+mneniee@users.noreply.github.com>
2024-01-11 20:32:16 +0300

    add: server, appwrite db config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }, 
-  modules: ['@pinia/nuxt','@nuxtjs/tailwindcss', 'shadcn-nuxt', '@formkit/auto-animate/nuxt', '@nuxt/image'],
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@formkit/auto-animate/nuxt",
+    "@nuxt/image",
+  ],
   shadcn: {
-    prefix: 'Ui',
-    componentDir: './components/ui'
+    prefix: "Ui",
+    componentDir: "./components/ui",
   },
   pinia: {
-    storesDirs: ['./store/**'],
+    storesDirs: ["./store/**"],
   },
-  runtimeConfig:{
-    public: {
-      appwriteKey: process.env.NUXT_APPWRITE_KEY
-    }
-  }
-})
-
+  runtimeConfig: {
+    appwriteKey: process.env.NUXT_APPWRITE_KEY,
+  },
+});
