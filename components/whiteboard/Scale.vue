d11fab53f54f10244e3054c96775b5e04826c0c4
mnenie <1alexpeshkov@gmail.com>
2024-03-11 21:56:30 +0300

    fix: styles in undo-redo + scale

--- components/whiteboard/Scale.vue
+++ components/whiteboard/Scale.vue
@@ -1,29 +1,40 @@
 <script setup lang="ts">
-import { Plus, Minus } from "lucide-vue-next";
+import { Plus, Minus } from 'lucide-vue-next';
 
 const emits = defineEmits<{
-  (e: 'minusZoom'): void,
-  (e: 'plusZoom'): void
-}>()
+  (e: 'minusZoom'): void;
+  (e: 'plusZoom'): void;
+}>();
 
 const props = defineProps<{
-  scale: number
-}>()
+  scale: number;
+}>();
 
-const canvasStore = useCanvasStore()
-
+const canvasStore = useCanvasStore();
 </script>
 <template>
   <div
     v-if="canvasStore.canvasSkeleton"
-    class="flex gap-2 items-center p-2 fixed bg-white rounded-md shadow-xl bottom-2 right-2 h-12"
+    class="absolute bottom-2 right-2 flex h-12 items-center rounded-md bg-white shadow-xl"
   >
-    <Minus @click="emits('minusZoom')" :size="20" :stroke-width="1.8" />
-    <span class="text-sm font-medium text-gray-950">{{ new Intl.NumberFormat("en-GB", {style: "percent"}).format(props.scale) }}</span>
-    <Plus @click="emits('plusZoom')" :size="20" :stroke-width="1.8" />
+    <div
+      @click="emits('minusZoom')"
+      class="mr-2 flex h-12 cursor-pointer items-center justify-center rounded-md p-2 active:bg-gray-100"
+    >
+      <Minus :size="20" :stroke-width="1.8" />
+    </div>
+    <span class="text-sm font-medium text-gray-950">{{
+      new Intl.NumberFormat('en-GB', { style: 'percent' }).format(props.scale)
+    }}</span>
+    <div
+      @click="emits('plusZoom')"
+      class="ml-2 flex h-12 cursor-pointer items-center justify-center rounded-md p-2 active:bg-gray-100"
+    >
+      <Plus :size="20" :stroke-width="1.8" />
+    </div>
   </div>
   <UiSkeleton
-    class="h-12 fixed bg-gray-200 rounded-md shadow-xl bottom-2 right-2 w-[92px]"
+    class="fixed bottom-2 right-2 h-12 w-[120px] rounded-md bg-gray-200 shadow-xl"
     v-else
   />
 </template>
--- components/whiteboard/UndoRedo.vue
+++ components/whiteboard/UndoRedo.vue
@@ -1,25 +1,68 @@
 <script setup lang="ts">
 import { Undo2, Redo2 } from 'lucide-vue-next';
 import { useCanvasStore } from '~/store/canvas.store';
-import type { UndoRedo } from '~/types/methods.type';
 
 const canvasStore = useCanvasStore();
 const emits = defineEmits<{
-  (e: 'undo'): void,
-  (e:'redo'): void
-}>()
+  (e: 'undo'): void;
+  (e: 'redo'): void;
+}>();
+
+const isActiveUndo = ref(false);
+const isActiveRedo = ref(false);
+
+const handleKeyPress = (event: KeyboardEvent) => {
+  if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
+    isActiveUndo.value = true;
+  } else if (
+    (event.ctrlKey || event.metaKey) &&
+    (event.key === 'y' || (event.shiftKey && event.key === 'Z'))
+  ) {
+    isActiveRedo.value = true;
+  }
+};
+
+const handleKeyUp = (event: KeyboardEvent) => {
+  if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
+    isActiveUndo.value = false;
+  } else if (
+    (event.ctrlKey || event.metaKey) &&
+    (event.key === 'y' || (event.shiftKey && event.key === 'Z'))
+  ) {
+    isActiveRedo.value = false;
+  }
+};
+
+watchEffect(() => {
+  if (window) {
+    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('keyup', handleKeyUp);
+  }
+});
 </script>
 
 <template>
   <div
     v-if="canvasStore.canvasSkeleton"
-    class="absolute bottom-2 right-32 flex h-12 items-center gap-5 rounded-md bg-white p-2 shadow-xl"
+    class="absolute bottom-2 right-36 flex h-12 items-center rounded-md bg-white shadow-xl"
   >
-    <Undo2 :size="20" :stroke-width="1.8" @click="emits('undo')" class="cursor-pointer" />
-    <Redo2 :size="20" :stroke-width="1.8" @click="emits('redo')" class="cursor-pointer" />
+    <div
+      @click="emits('undo')"
+      class="flex h-12 cursor-pointer items-center justify-center rounded-md pb-2 pl-2 pr-2.5 pt-2 active:bg-gray-100"
+      :class="{ 'bg-gray-100': isActiveUndo }"
+    >
+      <Undo2 :size="20" :stroke-width="1.8" />
+    </div>
+    <div
+      @click="emits('redo')"
+      class="flex h-12 cursor-pointer items-center justify-center rounded-md pb-2 pl-2.5 pr-2 pt-2 active:bg-gray-100"
+      :class="{ 'bg-gray-100': isActiveRedo }"
+    >
+      <Redo2 :size="20" :stroke-width="1.8" />
+    </div>
   </div>
   <UiSkeleton
     v-else
-    class="fixed bottom-2 right-32 h-12 w-[76px] rounded-md bg-gray-200 shadow-xl"
+    class="fixed bottom-2 right-36 h-12 w-[76px] rounded-md bg-gray-200 shadow-xl"
   />
 </template>
