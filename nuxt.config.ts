92b93e6482986723a48c979a3306e7cb9d870875
mnenie <1alexpeshkov@gmail.com>
2024-03-13 19:24:53 +0300

    add + done: seo optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  //seo
+  app: {
+    head: {
+      title: 'Boardify',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content:
+            'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover, shrink-to-fit=no'
+        },
+        {
+          name: 'title',
+          content: 'Boardify - virtual whiteboard for your own and collaborative solutions.'
+        },
+        {
+          name: 'description',
+          content:
+            'Boardify is a virtual whiteboard that lets you easy to express your thoughts in the form of drawings'
+        },
+        { name: 'keywords', content: 'Boardify, Whiteboard, Draw, Collaborative' },
+
+        //open graph
+        { property: 'og:site:name', content: 'Boardify' },
+        { property: 'og:type', content: 'website' },
+        {
+          property: 'og:title',
+          content: 'Boardify - virtual whiteboard for your own and collaborative solutions.'
+        },
+        {
+          property: 'og:description',
+          content:
+            'Boardify is a virtual whiteboard that lets you easy to express your thoughts in the form of drawings'
+        },
+        { property: 'og:image', content: './public/about/boardify.png' }
+      ]
+    }
+  },
   modules: [
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
@@ -23,13 +61,4 @@ export default defineNuxtConfig({
       firebaseApi: process.env.FIREBASE_API_KEY
     }
   },
-  app: {
-    head: {
-      title: 'Boardify',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover, shrink-to-fit=no' }
-      ]
-    }
-  }
 });
